dd66fc3765f28988fc0143b48e1febaa16226858
Khiew Shi Kai <khiewshikai@Khiews-MacBook-Pro.local>
2014-09-07 13:55:17 +0800

    added shi kai's files

---
+++ shikai/polymer_test2/client/templates/todo_list.js
@@ -0,0 +1,46 @@
+var toDoArray = [
+{
+	title: 'do number 1',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 2',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 3',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 4',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 5',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 6',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 7',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 8',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 9',
+	description: 'blah blah blah blah'
+},
+{
+	title: 'do number 10',
+	description: 'blah blah blah blah'
+}
+];
+
+Template.todoList.helpers({
+	toDoItems: toDoArray
+});
