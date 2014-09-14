d7cf4a626ddbb5d065fd6536de60c349749d79e5
Khiew Shi Kai <khiewshikai@Khiews-MacBook-Pro.local>
2014-09-14 17:23:44 +0800

    [sk][shikai] populate to do list

--- shikai/to_do/server/to_do_data.js
+++ shikai/to_do/server/to_do_data.js
@@ -19,4 +19,67 @@ if (ToDoClient.find().count() === 0) {
     date: '',
     completed: true
   });
+
+  ToDoClient.insert({
+    title: 'To do number 1',
+    description: 'abcdefg',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 2',
+    description: 'hijklmn',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 3',
+    description: 'opqrstuv',
+    date: '',
+    completed: true
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 1',
+    description: 'abcdefg',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 2',
+    description: 'hijklmn',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 3',
+    description: 'opqrstuv',
+    date: '',
+    completed: true
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 1',
+    description: 'abcdefg',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 2',
+    description: 'hijklmn',
+    date: '',
+    completed: false
+  });
+
+  ToDoClient.insert({
+    title: 'To do number 3',
+    description: 'opqrstuv',
+    date: '',
+    completed: true
+  });
 }
