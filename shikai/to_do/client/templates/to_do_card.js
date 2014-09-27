2c742351b2c6e8422a0311f5f45fbf80f7ab8b70
Shi Kai <khiewshikai@Khiews-MacBook-Pro.local>
2014-09-27 14:42:15 +0800

    [shikai] updated to do

--- shikai/to_do/client/templates/to_do_card.js
+++ shikai/to_do/client/templates/to_do_card.js
@@ -70,4 +70,86 @@ Template.toDoCard.events({
 
 Template.toDoCard.rendered = function () {
 	$('.editItemSubmit').addClass('hide');
+	 $('.content').on('scroll', function () {
+        scrollFunction();
+    });
 };
+
+function scrollFunction() {
+
+    var sticky1 = $('#stickyHeader1');
+    var sticky2 = $('#stickyHeader2');
+
+    // sticky1.css({
+    //     position: "static",
+    //     top: 0
+    // });
+    // sticky2.css({
+    //     position: "static",
+    //     top: 0
+    // });
+    // $('body').css({
+    //     "padding-top": 0
+    // });
+
+    var topOffset1 = sticky1.offset().top;
+    var topOffset2 = sticky2.offset().top;
+
+    console.log("STICKY 1 " + topOffset1);
+    console.log("SCROLLING" + $('.content').scrollTop());
+
+    var stickyHeight1 = sticky1.outerHeight();
+    var stickyHeight2 = sticky2.outerHeight();
+
+     var scrollHeight = $('.content').scrollTop();
+
+    if (topOffset1 <= scrollHeight && scrollHeight < topOffset2 - stickyHeight1) {
+        sticky1.css({
+            position: "absolute",
+            top: 0
+        });
+        sticky2.css({
+            position: "static",
+            top: 0
+        });
+        // $('body').css({
+        //     "padding-top": stickyHeight1
+        // });
+    } else if (scrollHeight >= topOffset2 - stickyHeight1 && scrollHeight < topOffset2) {
+        sticky1.css({
+            position: "absolute",
+            top: -(scrollHeight - (topOffset2 - stickyHeight1))
+        });
+        sticky2.css({
+            position: "absolute",
+            top: stickyHeight1 - (scrollHeight - (topOffset2 - stickyHeight1))
+        });
+        // $('body').css({
+        //     "padding-top": stickyHeight1 + stickyHeight2
+        // });
+    } else if (scrollHeight >= topOffset2) {
+        sticky1.css({
+            position: "static"
+        });
+        sticky2.css({
+            position: "absolute",
+            top: 0
+        });
+        // $('body').css({
+        //     "padding-top": stickyHeight2
+        // });
+    } else {
+        sticky1.css({
+            position: "static",
+            top: 0
+        });
+        sticky2.css({
+            position: "static",
+            top: 0
+        });
+        // $('body').css({
+        //     "padding-top": 0
+        // });
+    }
+
+}
